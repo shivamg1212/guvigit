41dd63ad77c17f1e78f2ae0442de9af7acffb8c7
shivam <35231376+shivamg1212@users.noreply.github.com>
2018-01-24 22:39:00 +0530

    Create integer.c

---
+++ integer.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+int a;
+printf("enter the number");
+scanf("%d",&a);
+if(a>0)
+{
+printf("POSITIVE");
+}
+else if(a<0)
+{
+printf("NEGATIVE");
+}
+else
+{
+printff("ZERO");
+}
+}
